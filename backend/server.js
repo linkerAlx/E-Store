b1192e7dd90448748bb8da8a59e2b4099b83611e
linkerAlx <linker222006766@gmail.com>
2024-09-19 15:38:04 +0300

    e-store project

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import  cookieparser from 'cookie-parser';
+import ProductRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cookieparser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/products",ProductRoutes);
+app.use("/api/cart",cartRoutes);
+app.use("/api/coupons",couponRoutes);
+app.use("/api/payments",paymentRoutes);
+app.use("/api/payments",analyticsRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+ app.listen(PORT,() => {
+    console.log("Server running on http://localhost:" +PORT); 
+    connectDB();
+ });
+ 
+
