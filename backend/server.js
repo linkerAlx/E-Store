e43632c68a3b40b17a1efe696ec6c6ff28f8cddd
linkerAlx <linker222006766@gmail.com>
2024-09-20 21:40:43 +0300

    fullstack e-store

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import  cookieparser from 'cookie-parser';
@@ -15,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieparser());
+app.use(cors());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/products",ProductRoutes);
@@ -22,6 +24,10 @@ app.use("/api/cart",cartRoutes);
 app.use("/api/coupons",couponRoutes);
 app.use("/api/payments",paymentRoutes);
 app.use("/api/payments",analyticsRoutes);
+
+
+
+
 
 
 if (process.env.NODE_ENV === "production") {
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
 	const { cart } = useCartStore();
 
 	return (
-		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-100'>
+		<header className='fixed top-0 left-0 w-full bg-yellow-950 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-100'>
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
-					<Link to='/' className='text-2xl font-bold text-emerald-100 items-center space-x-2 flex'>
-						Linker`store
+					<Link to='/' className='text-2xl font-bold text-emerald-100 items-center space-x-2 flex font-serif'>
+						Linker<br />store
 					</Link>
 
 					<nav className='flex flex-wrap items-center gap-4'>
